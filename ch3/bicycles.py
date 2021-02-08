330a019db4c4061abfcca6a8f2ebbcf392da6db8
ZhouXiongR <ZhouXiong1103@126.com>
2021-02-08 20:18:45 +0800

    update 2/8/2021

--- ch3/bicycles.py
+++ ch3/bicycles.py
@@ -5,3 +5,11 @@ bicycles=['trek','cannondale','redline','specialized']
 #print(bicycles[-2].title())
 message="my first bycicle was a " + bicycles[0].title() + "."
 print(message)
+print("The first three items in the list are:")
+print(bicycles[:3])
+
+print("\n Two items from the middle of the list are:")
+print(bicycles[1:3])
+
+print("\nThe last three items from the list are:")
+print(bicycles[-3:])
--- ch4/pizzas.py
+++ ch4/pizzas.py
@@ -2,3 +2,12 @@ pizzas=['SCpizza','CQpizza','BJpizza']
 for pizza in pizzas:
     print("I like " + pizza + " very much!")
 print("I really like pizza!")
+
+friend_pizzas=pizzas[:]
+pizzas.append("TJpizza")
+friend_pizzas.append("CDpizza")
+
+print("My favorite pizzas are:")
+print(pizzas)
+print("\nMy friend's favorite pizzas are:")
+print(friend_pizzas)
